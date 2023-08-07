883820f2d159380337971c9afcdeb60638ba6f1e
jasonasante <jason.asante@outlook.com>
2023-08-07 01:42:52 +0100

    feat: add experience section to portfolio

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -1,9 +1,111 @@
 import React from 'react'
 import './experience.css'
+import { LuVerified } from 'react-icons/lu'
 
 const Experience = () => {
     return (
-        <section id="experience">Experience</section>
+        <section id="experience">
+            <h5>Get To Know</h5>
+            <h2>My Experience</h2>
+            <div className="container experience-container">
+                <div className="front-end-experience">
+                    <h3>Front-End Development</h3>
+                    <div className="experience-content">
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>HTML</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>CSS</h4>
+                                <small className='text-light'>Intemediate</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>JavaScript</h4>
+                                <small className='text-light'>Intemediate</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>React</h4>
+                                <small className='text-light'>Intemediate</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div className="back-end-experience">
+                    <h3>Back-End Development</h3>
+                    <div className="experience-content">
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>Go</h4>
+                                <small className='text-light'>Intemediate</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>SQLite3</h4>
+                                <small className='text-light'>Intemediate</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>GraphQL</h4>
+                                <small className='text-light'>Beginner</small>
+                            </div>
+                        </article>
+
+                    </div>
+                </div>
+                <div className="other-experience">
+                    <h3>Other</h3>
+                    <div className="experience-content">
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>Visual Studio Code</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>Linux</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>Git</h4>
+                                <small className='text-light'>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience-card'>
+                            <LuVerified className="experience-icon" />
+                            <div>
+                                <h4>Restful Api</h4>
+                                <small className='text-light'>Intemediate</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div className='more-projects'>
+                <a href="#/projects" className='btn-primary'>Projects </a>
+            </div>
+        </section>
     )
 }
 
